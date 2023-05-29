da3ce271a59b0968975dd0746c4383bef4a22de1
Vulsan Bianca Maria <68532670+BiancaMariaVulsan@users.noreply.github.com>
2023-05-29 15:37:39 +0300

    5 window census

--- OpenCVApplication-VS2019_OCV451_basic/OpenCVApplication.cpp
+++ OpenCVApplication-VS2019_OCV451_basic/OpenCVApplication.cpp
@@ -7,21 +7,14 @@
 
 using namespace std;
 
-Scalar median(Mat_<uchar> window) {
-    Mat_<uchar> sorted;
-    cv::sort(window, sorted, SORT_ASCENDING);
-    int middle = window.total() / 2;
-    if (window.total() % 2 == 0) {
-        return (sorted(middle) + sorted(middle - 1)) / 2;
-    }
-    else {
-        return sorted(middle);
-    }
-}
+// cu fereastra mai mare si mat<int> pt dim 5
+// compar disparitatea cu cati pixeli la dif de < 2
 
+// incerc pe alta imagine mai mica sau o redirectionez pe asta
 
-Mat_<uchar> createCensusTransform(Mat_<uchar> image, int window_size)
+Mat_<int> createCensusTransform(Mat_<uchar> image, int window_size)
 {
+    cout << "Computing Census Transform" << endl;
     int half_window_size = window_size / 2;
 
     Mat_<uchar> resized_image;
@@ -31,7 +24,7 @@ Mat_<uchar> createCensusTransform(Mat_<uchar> image, int window_size)
     copyMakeBorder(resized_image, padded_image, half_window_size, half_window_size, half_window_size, half_window_size, BORDER_REFLECT);
 
 
-    Mat_<uchar> census = Mat::zeros(image.size(), CV_32SC1);
+    Mat_<int> census = Mat::zeros(image.size(), CV_32SC1);
 
     for (int row = 0; row < image.rows; row++)
     {
@@ -49,13 +42,13 @@ Mat_<uchar> createCensusTransform(Mat_<uchar> image, int window_size)
                     }
                 }
             }
-            uchar uchar_val = 0;
+            int int_val = 0;
 
-            // Convert binary vector to unsigned char
+            // Convert binary vector to int
             for (size_t i = 0; i < binary_string.size(); ++i) {
-                uchar_val |= binary_string[i] << i;
+                int_val |= binary_string[i] << i;
             }
-            census(row, col) = uchar_val;
+            census(row, col) = int_val;
         }
     }
 
@@ -74,6 +67,7 @@ int hammingDistance(string left_block, string right_block)
 
 Mat_<uchar> computeDisparityMap(Mat_<uchar> left_census, Mat_<uchar> right_census, int block_size, int max_disp)
 {
+    cout << "Computing disparity map" << endl;
     int half_block_size = block_size / 2;
 
     Mat_<uchar> padded_left_census, padded_right_census;
@@ -130,11 +124,26 @@ Mat_<uchar> computeDisparityMap(Mat_<uchar> left_census, Mat_<uchar> right_censu
     return disparity_map;
 }
 
+int compareImages(Mat_<uchar> myImg, Mat_<uchar> original) {
+    int count = 0;
+    for (int i = 0; i < min(myImg.rows, original.rows); i++) {
+        for (int j = 0; j < min(myImg.cols, original.cols); j++) {
+            if (abs(myImg(i, j) - original(i, j)) <= 2) {
+                count++;
+            }
+        }
+    }
+    return count / myImg.rows * myImg.cols; //procentage
+}
+
 
 int main()
 {
-    Mat_<uchar> img_left = imread("Images\\left.png", IMREAD_GRAYSCALE);
-    Mat_<uchar> img_right = imread("Images\\right.png", IMREAD_GRAYSCALE);    
+    //Mat_<uchar> img_left = imread("Images\\left.png", IMREAD_GRAYSCALE);
+    //Mat_<uchar> img_right = imread("Images\\right.png", IMREAD_GRAYSCALE);
+    Mat_<uchar> img_left = imread("Images\\view0.png", IMREAD_GRAYSCALE);
+    Mat_<uchar> img_right = imread("Images\\view1.png", IMREAD_GRAYSCALE);
+    Mat_<uchar> result = imread("Images\\disp1.png", IMREAD_GRAYSCALE);
     
     //imshow("img_left", img_left);
     //imshow("img_right", img_right);
@@ -142,27 +151,21 @@ int main()
 
     // Apply Census
     int window_size = 3;
-    Mat_<uchar> left_census;
-    Mat_<uchar> right_census;
+    Mat_<int> left_census;
+    Mat_<int> right_census;
 
-    try {
-        left_census = imread("Images\\Census_left.png", IMREAD_GRAYSCALE);
-        right_census = imread("Images\\Census_right.png", IMREAD_GRAYSCALE);
-    }
-    catch (Exception e) {
-        left_census = createCensusTransform(img_left, window_size);
-        right_census = createCensusTransform(img_right, window_size);
-    }
-
-
-    //imshow("left_census", left_census);
-    //imshow("right_census", right_census);
-    //waitKey(0);
+    left_census = createCensusTransform(img_left, window_size);
+    right_census = createCensusTransform(img_right, window_size);
+    imwrite("Images\\Census_left_view_3.png", left_census);
+    imwrite("Images\\Census_right_view_3.png", right_census);
 
     // Compute the disparity map based on the Hamming distance
     Mat_<uchar> disparity_map = computeDisparityMap(left_census, right_census, 3, 128);
  
     imshow("disparity_map", disparity_map);
     waitKey(0);
+    imwrite("Images\\disparity_map_view_3.bmp", disparity_map);
+
+    cout << compareImages(disparity_map, result);
 	return 0;
 }
